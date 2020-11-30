4d5897336145a0e76241eb3f481f53551630668d
Zei-Wei <71250782+Zei-Wei@users.noreply.github.com>
2020-11-30 14:53:33 +0800

    Add files via upload

---
+++ week12_王子維.py
@@ -0,0 +1,35 @@
+hello1 = "Hello World"
+hello2 = 'Hello Python'
+hello3 = "Shaquile O'Neil"
+
+s1 = "123"
+s2 = "456"
+s3 = s1 + s2
+print(s3)
+s4 = s1 * 2
+print(s4)
+
+s1 = 'An apple a day.'
+s1.capitalize()
+s1.title()
+s1.swapcase()
+s1.upper()
+s1.lower()
+
+numberlist = [1,2,3,4,5]
+charlist = ["1","2","3","4","5"]
+mixlist = [1,"2",3,"4",5]
+
+ Beason = "1995-1996"
+team = "Chicago Bulle"
+coach = "Phil Jackson"
+records = [72,10]
+starting_lineup = ["ron Harper","Michael Jordan","Scottie Pippen","Dennis Rodman", "Luc Longley"]
+won_championship = Ture
+
+best_chicago_bulls = list((season, team, coach, records,
+starting_lineup, won_championship))
+print(best_chicago_bulls)
+print(type(best_chicago_bulls))
+# indexing 索引
+print(best_chicago_bulls[0])
